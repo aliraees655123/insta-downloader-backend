9b9fccd65085ba1e5d207fe96cb0406b6fb0bca5
aliraees655123 <96068596+aliraees655123@users.noreply.github.com>
2023-01-28 10:32:31 +0500

    ss

--- server.js
+++ server.js
@@ -14,6 +14,42 @@ app.use(
 )
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: false, limit: '50mb' }))
+
+
+
+//////////
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+app.use(
+  cors({
+    origin: "*",
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
+app.options(
+  "*",
+  cors({
+    origin: "*",
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
+///////////
+
+
+
+
+
+
+
+
 const server = http.createServer(app)
 const io = require('socket.io')(server, {
   cors: {
